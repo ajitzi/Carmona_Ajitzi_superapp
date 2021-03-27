05a31417d7d044b3846d7b7bed2933d25201d036
Ajitzi <ajitzi.carmona@outlook.com>
2021-03-27 11:11:33 +0100

    Mon premier commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const headerIcon = document.getElementById('headerIcon');
+const menu = document.getElementById('menu');
+
+headerIcon.addEventListener('click', function() {
+    menu.classList.toggle('pop')
+});
